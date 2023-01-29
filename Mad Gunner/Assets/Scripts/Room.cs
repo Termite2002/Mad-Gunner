37e109832e5323aec0df2fae94fbca21947237e6
Termite2002 <motsach1732002@gmail.com>
2023-01-29 22:23:30 +0700

    door, design

--- Mad Gunner/Assets/Scripts/Room.cs
+++ Mad Gunner/Assets/Scripts/Room.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool closeWhenEntered, openWhenEnemiesCleared;
+
+    public GameObject[] doors;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private bool roomActive;
     void Start()
     {
         
@@ -13,13 +19,50 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemies.Count > 0 && roomActive && openWhenEnemiesCleared)
+        {
+            for(int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (enemies.Count == 0)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+
+                    closeWhenEntered = false;
+                }
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             CameraController.instance.ChangeTarget(transform);
+
+            if (closeWhenEntered)
+            {
+                foreach(GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
+
+            roomActive = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            roomActive = false;
         }
     }
 }
